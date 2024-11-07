d3ff1dcbe0bd983f7602278545a83714041cbc7a
Abdirizal024 <154967196+Abdirizal024@users.noreply.github.com>
2024-11-07 20:13:24 +0800

    Update contactus.js

--- contactus.js
+++ contactus.js
@@ -1,20 +1,14 @@
 
 
-
 function sendEmail() {
     Email.send({
         Host: "smtp.gmail.com",
-        Port: "465",
-        Secure: true,
         Username: "rizalbjm7788@gmail.com",
         Password: "eobkbyunwcvcoxcq",
         To: 'rizalbjm7788@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: "New Inquery on iVishMusic.com",
-        Body : "Name : "+ document.getElementById("name").value
-         +"<br> Email : "+ document.getElementById("email").value
-         +"<br> Phone No. : "+ document.getElementById("phone").value
-         +"<br> Messege : "+ document.getElementById("messege").value
+        From : "rizalbjm679@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
     }).then(
         message => alert("Message Sent Succesfully. Thank You.")
     );
